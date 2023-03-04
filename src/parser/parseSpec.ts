24a858f2fb0a2f1403b174ce9f689fdb491af021
Marco Muser <marco.muser@posteo.de>
2023-03-04 15:35:17 +0100

    Fix fn name

--- src/parser/parseSpec.ts
+++ src/parser/parseSpec.ts
@@ -1,9 +1,9 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { Document } from "../types.js";
-import { extractOperations } from "./getOperations.js";
+import { getOperations } from "./getOperations.js";
 
 export const parseSpec = async (pathToSpec: string) => {
   const parser = new SwaggerParser();
   const dereferencedSpec = (await parser.dereference(pathToSpec)) as Document;
-  return extractOperations(dereferencedSpec);
+  return getOperations(dereferencedSpec);
 };
