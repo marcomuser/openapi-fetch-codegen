4b15c26724004602e6937f74491893382146b933
Marco Muser <marco.muser@posteo.de>
2023-03-04 08:53:56 +0100

    Add more types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,13 @@
-import type { OpenAPIV3 } from "openapi-types";
+import { OpenAPIV3 } from "openapi-types";
 
 export type Document = OpenAPIV3.Document;
 
 export type Operation = OpenAPIV3.OperationObject;
+
+export type Parameter = OpenAPIV3.ParameterObject;
+
+export type RequestBody = OpenAPIV3.RequestBodyObject;
+
+export type Responses = OpenAPIV3.ResponsesObject;
+
+export type Response = OpenAPIV3.ResponseObject;
