af0432c74e3377bf83c305011948634bc826048f
Marco Muser <marco.muser@posteo.de>
2023-03-05 17:49:49 +0100

    Adjust spacing

--- src/printer/fetchFns/body/printFnBody.ts
+++ src/printer/fetchFns/body/printFnBody.ts
@@ -3,12 +3,11 @@ import { printOptions } from "./printOptions.js";
 import { printReturn } from "./printReturn.js";
 import { printUrl } from "./printUrl.js";
 
-export const printFnBody = (operation: ExtractedOperation) => {
-  return `{
+export const printFnBody = (operation: ExtractedOperation) =>
+  `{
     ${printOptions(operation)}
-
-    ${printUrl(operation)}
-
-    ${printReturn(operation)}
+  
+  ${printUrl(operation)}
+    
+  ${printReturn(operation)}
   };`;
-};
