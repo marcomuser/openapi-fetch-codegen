d5f998909a57a9ba9bc64efba3c6418d442c52ba
Marco Muser <marco.muser@posteo.de>
2023-03-05 17:39:22 +0100

    Add replacePathParams fn

--- src/printer/fetchFns/body/printUrl.ts
+++ src/printer/fetchFns/body/printUrl.ts
@@ -1,7 +1,17 @@
 import type { ExtractedOperation } from "../../../parser/getOperations.js";
 
 export const printUrl = ({ path }: ExtractedOperation) => {
+  const pathWithPathParameters = replacePathParams(path);
+
   return `const searchParams = new URLSearchParams(params.query);
-  const url = new URL("/pet/params.path.petId", baseUrl);
+  const url = new URL(${pathWithPathParameters}, baseUrl);
   url.search = searchParams.toString();`;
+};
+
+const replacePathParams = (path: string) => {
+  const paramRegex = /\{([^}]+)\}/g;
+  return path.replace(
+    paramRegex,
+    (_, paramName) => `\${params.path.${paramName}}`
+  );
 };
