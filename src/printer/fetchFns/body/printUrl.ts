d3f8c34676733ba81443e48e6a0d82b86ea1e13e
Marco Muser <marco.muser@posteo.de>
2023-03-05 18:17:35 +0100

    Fix regex

--- src/printer/fetchFns/body/printUrl.ts
+++ src/printer/fetchFns/body/printUrl.ts
@@ -10,7 +10,7 @@ export const printUrl = ({ path }: ExtractedOperation) => {
 
 const replacePathParams = (path: string) => {
   // matches any substring in a string that is surrounded by curly braces
-  const paramRegex = /\{([^}]+)\}/;
+  const paramRegex = /\{([^}]+)\}/g;
   return path.replaceAll(
     paramRegex,
     (_, paramName: string) => `\${params.path.${paramName}}`
