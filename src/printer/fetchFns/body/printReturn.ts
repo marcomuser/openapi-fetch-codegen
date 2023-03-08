73b2dd77409e1c18ddb9298fcea1e7e545a17dc8
Marco Muser <marco.muser@posteo.de>
2023-03-08 17:17:10 +0100

    Add printSwitchStatement

--- src/printer/fetchFns/body/printReturn.ts
+++ src/printer/fetchFns/body/printReturn.ts
@@ -1,24 +1,56 @@
 import type { ExtractedOperation } from "../../../parser/getOperations.js";
 import { RES_CONTENT_TYPE_DICT } from "../../../utils/constants.js";
 
-export const printReturn = ({
-  operationId,
-  resWithSortedContentTypes,
-}: ExtractedOperation) => {
-  const okResponse = resWithSortedContentTypes["200"];
+export const printReturn = (operation: ExtractedOperation) => {
   return `const response = await fetch(url, options);
-
-  return {
-    response,
-    data: (${getResType(
-      okResponse
-    )} as operations[${operationId}]["responses"]["200"]["content"]["application/json"],
-  };`;
+  
+  ${printSwitchStatement(operation)}`;
 };
 
-const getResType = (sortedResContentTypes: string[]) => {
-  if (isHandledContentType(sortedResContentTypes[0])) {
-    return `(await response${RES_CONTENT_TYPE_DICT[sortedResContentTypes[0]]})`;
+const printSwitchStatement = ({
+  operationId,
+  resWithPreferredContentType,
+}: ExtractedOperation) => {
+  let switchStatement = `switch (response.status) {
+    `;
+
+  for (const status in resWithPreferredContentType) {
+    if (resWithPreferredContentType[status]) {
+      switchStatement += `case ${status}: 
+        return {
+          response,
+          data: ${getDataValue(
+            resWithPreferredContentType[status] as string
+          )} as operations["${operationId}"]["responses"]["${status}"]["content"]["${
+        resWithPreferredContentType[status]
+      }"],
+        };
+
+        `;
+    } else {
+      switchStatement += `case ${status}:
+        return {
+          response,
+          data: undefined,
+        };
+
+      `;
+    }
+  }
+
+  switchStatement += `default:
+    return {
+      response,
+      data: undefined,
+    };
+  };`;
+
+  return switchStatement;
+};
+
+const getDataValue = (preferredContentType: string) => {
+  if (isHandledContentType(preferredContentType)) {
+    return `(await response${RES_CONTENT_TYPE_DICT[preferredContentType]})`;
   }
 
   return "response.body";
