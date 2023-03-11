1fb2e7984f7cc1fae09620cca6fb70393e09fe7a
Marco Muser <marco.muser@posteo.de>
2023-03-11 09:23:26 +0100

    Handle more complex query params cases

--- src/printer/fetchFns/body/printUrl.ts
+++ src/printer/fetchFns/body/printUrl.ts
@@ -6,7 +6,7 @@ export const printUrl = ({ path, hasQueryParams }: ExtractedOperation) => {
   return !hasQueryParams
     ? `const url = new URL(\`${replacedPath}\`, baseUrl);`
     : `const url = new URL(\`${replacedPath}\`, baseUrl);
-    const searchParams = new URLSearchParams(params.query);
+    ${searchParams}
     url.search = searchParams.toString();`;
 };
 
@@ -18,3 +18,16 @@ const replacePathParams = (path: string) => {
     (_, paramName: string) => `\${params.path.${paramName}}`
   );
 };
+
+const searchParams = `const searchParams = new URLSearchParams();
+  for (const [key, value] of Object.entries(params.query)) {
+    if (Array.isArray(value)) {
+      for (const item of value) {
+        searchParams.append(key, item);
+      }
+    } else if (typeof value === "object") {
+      searchParams.set(key, JSON.stringify(value));
+    } else {
+      searchParams.set(key, value);
+    }
+  }`;
