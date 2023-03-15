99f50d71bfa8653936ef7493e34228dfe05ca8d1
Marco Muser <marco.muser@posteo.de>
2023-03-15 18:11:19 +0100

    Handle more content types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,18 +16,24 @@ export const PREFERRED_REQ_CONTENT_TYPES = [
 ];
 
 export const REQ_BODY_CONTENT_TYPE_DICT = {
+  "*": "JSON.stringify(params.requestBody)",
   "application/*": "JSON.stringify(params.requestBody)",
   "application/json": "JSON.stringify(params.requestBody)",
   "application/x-www-form-urlencoded":
     "new URLSearchParams(params.requestBody)",
-  "*": "JSON.stringify(params.requestBody)",
+  "text/html": "params.requestBody",
+  "text/plain": "params.requestBody",
+  "text/plain charset=utf-8": "params.requestBody",
   "multipart/form-data": "new FormData(params.requestBody)",
 } as const;
 
 export const PREFERRED_RES_CONTENT_TYPES = ["application/json", "text/plain"];
 
 export const RES_CONTENT_TYPE_DICT = {
+  "*": ".json()",
+  "application/*": ".json()",
   "application/json": ".json()",
   "text/plain": ".text()",
+  "text/plain charset=utf-8": ".text()",
   "text/html": ".text()",
 } as const;
